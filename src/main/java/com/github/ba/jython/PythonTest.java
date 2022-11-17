3402d224527b8a2fd4dc3746f4b4d25457f4c87b
wangxiao <wxiao1002@yeah.net>
2022-11-17 16:32:22 +0800

    python test

---
+++ src/main/java/com/github/ba/jython/PythonTest.java
@@ -0,0 +1,44 @@
+package com.github.ba.jython;
+
+import org.python.core.*;
+import org.python.util.PythonInterpreter;
+
+import javax.script.*;
+import java.io.ByteArrayInputStream;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.HashMap;
+
+/**
+ * @author wang xiao
+ * date 2022/11/17
+ */
+public class PythonTest {
+    public static void main(String[] args) throws ScriptException {
+        //首先调用python的解释器
+        System.out.println("0"+Instant.now());
+        PythonInterpreter interpreter = new PythonInterpreter();
+        //选择执行的的Python语句
+        String source = "def covert(state):\n" +
+                "    return state.id";
+        System.out.println("1"+Instant.now());
+
+        interpreter.execfile(new ByteArrayInputStream(source.getBytes(StandardCharsets.UTF_8)));
+        System.out.println("2"+Instant.now());
+
+        PyFunction pyFunction = interpreter.get("covert", PyFunction.class);
+
+        HashMap<String,Object> v = new HashMap<String,Object>();
+        v.put("id",1);
+
+        PyObject pyObject = pyFunction.__call__(PyJavaType.wrapJavaObject(v));
+        System.out.println(pyObject);
+        System.out.println("3"+Instant.now());
+
+        PyObject pyobj = pyFunction.__call__();
+        System.out.println("4"+Instant.now());
+        System.out.println("the anwser is: " + pyobj);
+
+    }
+}
